f89373f1270aca5c4fdafff6474cb5abbb8ecee9
ilya <ilyaandreev2010@gmail.com>
2021-03-10 16:25:05 +0300

    Remove TURN TLS support

--- lib/geobalance/geobalance.go
+++ lib/geobalance/geobalance.go
@@ -14,11 +14,11 @@ type turnInfo struct {
 
 const (
 	// FRA
-	fraURL = "turns:prod-turn-fra.airtap.dev:3478"
+	fraURL = "turn:prod-turn-fra.airtap.dev:443"
 	fraEnv = "TURN_FRA_KEY"
 
 	// SFO
-	sfoURL = "turns:prod-turn-sfo.airtap.dev:3478"
+	sfoURL = "turn:prod-turn-sfo.airtap.dev:443"
 	sfoEnv = "TURN_SFO_KEY"
 )
 
